6db0ca173dc403fe25d97deda762a060d4da7308
czelabueno <c.zelabueno@gmail.com>
2024-06-24 00:29:43 -0500

    updating transition classes

--- src/main/java/dev/langchain4j/workflow/transition/Transition.java
+++ src/main/java/dev/langchain4j/workflow/transition/Transition.java
@@ -1,8 +1,10 @@
 package dev.langchain4j.workflow.transition;
 
 import dev.langchain4j.workflow.node.Node;
-import dev.langchain4j.workflow.WorkflowState;
+import dev.langchain4j.workflow.WorkflowStateName;
+import lombok.Getter;
 
+@Getter
 public class Transition<T> {
     private final Node<T, ?> from;
     private final Object to; // Can be Node<T,?> or WorflowState
@@ -10,14 +12,6 @@ public class Transition<T> {
     public Transition(Node<T, ?> from, Object to) {
         this.from = from;
         this.to = to;
-    }
-
-    public Node<T, ?> getFrom() {
-        return from;
-    }
-
-    public Object getTo() {
-        return to;
     }
 
     public static <T> Transition<T> from(Node<T, ?> from, Object to) {
@@ -28,8 +22,8 @@ public class Transition<T> {
     public String toString() {
         if (to instanceof Node) {
             return from.getName() + " -> " + ((Node<T, ?>) to).getName();
-        } else if (to instanceof WorkflowState) {
-            return from.getName() + " -> " + ((WorkflowState) to).toString();
+        } else if (to instanceof WorkflowStateName) {
+            return from.getName() + " -> " + ((WorkflowStateName) to).toString();
         }
         return "";
     }
