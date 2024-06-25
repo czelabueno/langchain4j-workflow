7e08c2ef4827a94f07cc281bcee3ad3dba1b9570
czelabueno <c.zelabueno@gmail.com>
2024-06-24 23:27:56 -0500

    Adding support to START state enum to transtion

--- src/main/java/dev/langchain4j/workflow/transition/Transition.java
+++ src/main/java/dev/langchain4j/workflow/transition/Transition.java
@@ -3,28 +3,41 @@ package dev.langchain4j.workflow.transition;
 import dev.langchain4j.workflow.node.Node;
 import dev.langchain4j.workflow.WorkflowStateName;
 import lombok.Getter;
+import lombok.NonNull;
 
 @Getter
 public class Transition<T> {
-    private final Node<T, ?> from;
+    private final Object from; // // Can be Node<T,?> or WorflowState
     private final Object to; // Can be Node<T,?> or WorflowState
 
-    public Transition(Node<T, ?> from, Object to) {
+    public Transition(@NonNull Object from, @NonNull Object to) {
+        if (from == WorkflowStateName.END) {
+            throw new IllegalArgumentException("Cannot transition from an END state");
+        }
+        if (to == WorkflowStateName.START) {
+            throw new IllegalArgumentException("Cannot transition to a START state");
+        }
         this.from = from;
         this.to = to;
     }
 
-    public static <T> Transition<T> from(Node<T, ?> from, Object to) {
+    public static <T> Transition<T> from(Object from, Object to) {
         return new Transition<>(from, to);
     }
 
     @Override
     public String toString() {
-        if (to instanceof Node) {
-            return from.getName() + " -> " + ((Node<T, ?>) to).getName();
-        } else if (to instanceof WorkflowStateName) {
-            return from.getName() + " -> " + ((WorkflowStateName) to).toString();
+        String transition = "";
+        if (from instanceof Node) {
+            transition = ((Node<T, ?>) from).getName() + " -> ";
+        } else if (from instanceof WorkflowStateName) {
+            transition = ((WorkflowStateName) from).toString() + " -> ";
         }
-        return "";
+        if (to instanceof Node) {
+            transition = transition + ((Node<T, ?>) to).getName();
+        } else if (to instanceof WorkflowStateName) {
+            transition = transition + ((WorkflowStateName) to).toString();
+        }
+        return transition;
     }
 }
